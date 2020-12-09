fb8f736958d1e2a1b2e1d32ed07b48d8086fbdc2
IntegralTeam <repo@sfxdx.ru>
2020-12-09 12:18:00 +0700

    fixed can't find atifacts on coverage

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+
 module.exports = {
-  skipFiles: ['Migrations.sol', 'Mintable.sol']
+  skipFiles: ['Migrations.sol', 'Mintable.sol'],
+  onCompileComplete: function () {
+    fs.copyFileSync('./node_modules/@uniswap/v2-periphery/build/WETH9.json', '.coverage_artifacts/contracts/WETH9.json');
+    fs.copyFileSync('./node_modules/@uniswap/v2-periphery/build/UniswapV2Router02.json', '.coverage_artifacts/contracts/UniswapV2Router02.json');
+    fs.copyFileSync('./node_modules/@uniswap/v2-core/build/UniswapV2Pair.json', '.coverage_artifacts/contracts/UniswapV2Pair.json');
+    fs.copyFileSync('./node_modules/@uniswap/v2-core/build/UniswapV2Factory.json', '.coverage_artifacts/contracts/UniswapV2Factory.json');
+  }
 };
