1ab913ad8133acac627194ed43690ed77732d8b3
IntegralTeam <repo@sfxdx.ru>
2020-12-10 11:26:08 +0700

    added TODOs

--- contracts/UNICToken.sol
+++ contracts/UNICToken.sol
@@ -8,18 +8,22 @@ import "./Interfaces.sol";
 contract UNICToken is IUnicToken, ERC20, Ownable {
     using SafeMath for uint256;
 
+    // TODO start time of what?
     uint256 public startTime;
 
     mapping(address => bool) internal _blacklistedAddresses;
 
     address internal _auctionAddress;
+    // TODO not needed burn\mint possible only by auction
     mapping (address => bool) internal _burnerAddresses;
 
+    // TODO isBurner should be changed to onlyAuction, mint\burn should be available only from auction
     modifier isBurner() {
         require(_burnerAddresses[_msgSender()], "Caller is not burner");
         _;
     }
 
+    // TODO isBlacklisted -> onlyIfNotBlacklisted, should return true if address in blacklist, overwise should be renamed (onlyIfNotBlacklisted)
     modifier isBlacklisted(address account) {
         require(!_blacklistedAddresses[account], "In black list");
         _;
@@ -30,6 +34,7 @@ contract UNICToken is IUnicToken, ERC20, Ownable {
         startTime = now;
     }
 
+    // TODO getIsBlackListed -> isBlacklisted
     function getIsBlackListed(address account) external override view returns (bool) {
         if (_blacklistedAddresses[account]) {
             return true;
@@ -45,12 +50,14 @@ contract UNICToken is IUnicToken, ERC20, Ownable {
         return _burnerAddresses[account];
     }
 
+    // TODO not needed
     function addBurner(address account) external onlyOwner {
         require(!_burnerAddresses[account], "Already burner");
         require(account != address(0), "Cant add zero address");
         _burnerAddresses[account] = true;
     }
 
+    // TODO not needed
     function removeBurner(address account) external onlyOwner {
         require(_burnerAddresses[account], "Isnt burner");
         _burnerAddresses[account] = false;
@@ -61,14 +68,17 @@ contract UNICToken is IUnicToken, ERC20, Ownable {
         _mint(_auctionAddress, amount);
     }
 
+    // TODO require _auctionAddress === msgsender
     function burn(uint256 amount) public isBurner override {
         _burn(_auctionAddress, amount);
     }
 
+    // TODO not needed
     function addToBlacklist(address account) public onlyOwner isBlacklisted(account) {
         _blacklistedAddresses[account] = true;
     }
 
+    // TODO not needed
     function rempoveFromBlacklist(address account) public onlyOwner {
         require(_blacklistedAddresses[account], "Not blacklisted");
         delete _blacklistedAddresses[account];
