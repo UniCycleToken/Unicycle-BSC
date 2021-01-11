2cf8265cbfb75dce9b78530fc416a4892a875bc0
Tranquil-Flow <66773372+Tranquil-Flow@users.noreply.github.com>
2021-01-12 00:38:24 +1000

    Implement LP Token Lock
    This code was adapted from CORE contracts. Some minor edits were needed, as the CORE contracts have their token pair defined in their LGE where as CYCLE will not have an LGE. In the CORE contract, the code used is:
    
    In the transfer function:
    
    (At the beginning of the function) 
            uint _LPSupplyOfCYCLEETHPairTotal = IERC20(tokenPair).totalSupply();
            if(sender == tokenPair) {
                require(lastTotalSupplyOfCYCLEETHLPTokens <= _LPSupplyOfCYCLEETHPairTotal, "Liquidity withdrawals forbidden for CYCLE/ETH pair");
            }
    
    TRANSFER CODE HERE
    
    (At the very end of the transfer function)
            lastTotalSupplyOfDFGETHLPTokens = _LPSupplyOfDFGETHPairTotal;
    
    
    Calling the sync function is what locks the LP tokens. After calling this, the LP will no longer be unwrappable. 
    
        function sync() internal onlyOwner {
            uint _LPSupplyOfDFGETHPairTotalPairTotal = IERC20(tokenUniswapPairDFGETH).totalSupply();
            lastTotalSupplyOfDFGETHLPTokens = _LPSupplyOfDFGETHPairTotalPairTotal;
        }

--- contracts/CYCLEToken.sol
+++ contracts/CYCLEToken.sol
@@ -1,9 +1,15 @@
 pragma solidity >= 0.6.0 < 0.7.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./Interfaces.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/ownership/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 
+interface ICycleToken is IERC20 {
+    function mint(uint256 amount) external;
+    function burn(uint256 amount) external;
+    function isBlacklisted(address account) view external returns (bool);
+    function setAuction(address account) external;
+}
 
 contract CYCLEToken is ICycleToken, ERC20, Ownable {
     using SafeMath for uint256;
@@ -11,6 +17,7 @@ contract CYCLEToken is ICycleToken, ERC20, Ownable {
     mapping(address => bool) private _blacklistedAddresses;
 
     address private _auctionAddress;
+    address public tokenPair;
 
     modifier onlyAuction() {
         require(_msgSender() == _auctionAddress, "Caller is not auction");
@@ -40,6 +47,15 @@ contract CYCLEToken is ICycleToken, ERC20, Ownable {
     function burn(uint256 amount) external override onlyAuction {
         _burn(_auctionAddress, amount);
     }
+    
+    function transfer(uint256 amount) external override {
+        uint _LPSupplyOfCYCLEETHPairTotal = IERC20(tokenPair).totalSupply();
+        if(sender == tokenPair) {
+            require(lastTotalSupplyOfCYCLEETHLPTokens <= _LPSupplyOfCYCLEETHPairTotal, "Liquidity withdrawals forbidden for CYCLE/ETH pair");
+        }
+        _transfer(amount);
+        lastTotalSupplyOfDFGETHLPTokens = _LPSupplyOfDFGETHPairTotal;
+    }
 
     function addToBlacklist(address account) external onlyOwner onlyIfNotBlacklisted(account) {
         _blacklistedAddresses[account] = true;
@@ -52,5 +68,11 @@ contract CYCLEToken is ICycleToken, ERC20, Ownable {
 
     function _beforeTokenTransfer(address from, address, uint256) internal override onlyIfNotBlacklisted(from) {
         // solium-disable-previous-line no-empty-blocks
+    }
+
+    function sync(address _tokenPair) external onlyOwner {
+        tokenPair = _tokenPair;
+        uint _LPSupplyOfDFGETHPairTotalPairTotal = IERC20(tokenUniswapPairDFGETH).totalSupply();
+        lastTotalSupplyOfDFGETHLPTokens = _LPSupplyOfDFGETHPairTotalPairTotal;
     }
 }
